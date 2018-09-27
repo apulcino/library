1e2cab8948abca61ec66b66070b6a0ab890bde4a
pulcino <alfredo.pulcino@afp.com>
2018-09-27 17:03:15 +0200

    MAJ Consul

--- consulMgr.js
+++ consulMgr.js
@@ -83,10 +83,61 @@ var consulMgr = {
                         "id": result[name].ID
                     });
                 }
+                // [
+                //     {"address":"158.50.163.7","port":8080,"name":"APIGateway","url":"http://158.50.163.7:8080","id":"APIGateway_8080"},
+                //     {"address":"158.50.163.7","port":51102,"name":"authent-v1","url":"http://158.50.163.7:51102","id":"authent-v1_51102"}
+                // ]
                 callbackOK(services);
             }
         });
+    },
+    HealthyServices: [],
+    /**
+     * convert public url to microservice local url
+     * @param {string} __reqUrl - The public url to convert to microservice private url.
+     * @param {requestCallback} _callbackErr - The callback that handles the error.
+     * @param {requestCallback} _callbackOK - The callback that handles the success.
+    */
+    Resolve: function (_reqUrl, _callbackErr, _callbackOK) {
+        this.HealthyServices = [];
+        let reqUrl = _reqUrl;
+        let callbackErr = _callbackErr || function () { };
+        let callbackOK = _callbackOK || function () { };
+        if (0 === this.HealthyServices.length) {
+            this.GetAllHealthyServices(
+                (err) => {
+                    callbackErr(err);
+                },
+                (services) => {
+                    this.HealthyServices = services;
+                    callbackOK(ResolveUrl(reqUrl, this.HealthyServices));
+                });
+        } else {
+            callbackOK(ResolveUrl(reqUrl, this.HealthyServices));
+        }
     }
+}
+
+/**
+ * convert public url to microservice local url
+ * @param {string} __reqUrl - The public url to convert to microservice private url Ex: "/authent/v1/user/login"
+ * Retourne : http://158.50.163.7:51102/api/v1/user/login
+*/
+function ResolveUrl(reqUrl, HealthyServices) {
+    // [
+    //     {"address":"158.50.163.7","port":8080,"name":"APIGateway","url":"http://158.50.163.7:8080","id":"APIGateway_8080"},
+    //     {"address":"158.50.163.7","port":51102,"name":"authent-v1","url":"http://158.50.163.7:51102","id":"authent-v1_51102"}
+    // ]
+    let token = reqUrl.split("/");
+    let reqSrvName = token[1] + "-" + token[2];
+    let selService = HealthyServices.find(service => {
+        if (service.name === reqSrvName) {
+            service.realUrl = reqUrl.replace(token[1], "api");
+            console.log('ResolveUrl : ' + reqUrl + " => " + service.realUrl);
+            return true;
+        }
+    });
+    return selService;
 }
 
 exports.consulMgr = consulMgr;
